886047f30577cbe537a8ef96cef5240edb009a11
samiksha-ambastha1205 <samikshaambastha9308@gmail.com>
2024-09-18 13:34:17 +0530

    Create PDF6.py

---
+++ Pdf_q/PDF6.py
@@ -0,0 +1,11 @@
+def flatten(x):
+    result = []
+    for item in x:
+        if isinstance(item , list):
+            result.extend(flatten(item))
+        else:
+            result.append(item)
+    return result
+x=eval(input())
+flattened = flatten(x)
+print(flattened)
